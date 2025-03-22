d147682403c01136247d73c74707b6b110870abb
AmanWalia-2001 <amanwalia00128@gmail.com>
2025-03-22 11:50:37 +0530

    changes in vercel.jjson and vitteconfig

--- dreamers/vite.config.js
+++ dreamers/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',  // 👈 Ensures correct path resolution for Vercel
+  build: {
+    outDir: 'dist', // Ensures Vite outputs build files to "dist"
+  }
 })
