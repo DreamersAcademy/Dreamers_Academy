c2068622c879caedd792bda4e091bf788079145a
AmanWalia-2001 <amanwalia00128@gmail.com>
2025-04-06 14:07:33 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -15,10 +15,7 @@ app.use(cors({
 
 require("dotenv").config(); // Load environment variables
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
