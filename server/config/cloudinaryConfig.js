be58a4f5aa3382b07b97b21fad85ca3bd11280fd
AmanWalia-2001 <amanwalia00128@gmail.com>
2025-04-07 16:25:52 +0530

    chnages

--- server/config/cloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-  cloud_name: 'ddunj6cag',
-  api_key: '353566348911824',
-  api_secret: '7mzCvCfowMU2nuDGz9SceYCa8bA',
-});
-
-module.exports = cloudinary;
